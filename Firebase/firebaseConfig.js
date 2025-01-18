bf737f432516aadbf8c2034e190df02590105449
Teo Cristante <teo.cristante@gmail.com>
2025-01-17 23:10:15 -0500

    Added env for 'secrets'

--- Firebase/firebaseConfig.js
+++ Firebase/firebaseConfig.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAXQcuYkr51CiSGt9TpCbpAESJlk_biejo",
-  authDomain: "unseen-83bae.firebaseapp.com",
-  projectId: "unseen-83bae",
-  storageBucket: "unseen-83bae.firebasestorage.app",
-  messagingSenderId: "66199453974",
-  appId: "1:66199453974:web:b0536bdd9ca98841b75037"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
