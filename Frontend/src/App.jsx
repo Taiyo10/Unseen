1c9f0ebf26f32313fefb9e67b6d83630f0b2a694
Allenreinoso28 <157051548+Allenreinoso28@users.noreply.github.com>
2025-01-17 21:06:34 -0500

    implement page function

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,32 @@
-// import { useState } from 'react'
-
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import Explore from "./pages/Explore/Explore";
+import Game from "./pages/Game/Game";
+import "./App.css";
 
 function App() {
-
   return (
-    <div> hello </div>
-  )
+    <Router>
+      <div>
+        <nav className="tab-bar">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/aboutus">About</Link></li>
+            <li><Link to="/game">Services</Link></li>
+            <li><Link to="/explore">Contact</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/game" element={<Game />} />
+          <Route path="/explore" element={<Explore />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
